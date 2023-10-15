6ab082cd9431faf01ab4b94554ed16d7af12b1d1
pjayas <115007528+pjayas@users.noreply.github.com>
2023-10-16 00:21:23 +0900

    update

--- Assets/Scripts/RedLight.cs
+++ Assets/Scripts/RedLight.cs
@@ -13,31 +13,21 @@ public class RedLight : MonoBehaviour
     {
         StartCoroutine(Blink());
     }
-
     IEnumerator Blink()
     {
         while (true)
         {
-            Debug.Log("while문 시작");
-
             waitSec = Random.Range(5, 10);
-
             if (isWork == false)
             {
                 PointLight.SetActive(true);
                 isWork = true;
-
-                Debug.Log("켜졌다");
-
             }
             else
             {
                 PointLight.SetActive(false);
                 isWork = false;
-                Debug.Log("꺼졌다");
-
             }
-            Debug.Log("if문 끝");
             yield return new WaitForSeconds(waitSec * 0.1f);
 
         }
