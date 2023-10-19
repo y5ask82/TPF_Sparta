072620afafa9d99b1f951d3ddc07afcacf264617
Nightshadow0911 <whqudgh0911@gmail.com>
2023-10-19 14:06:07 +0900

    [update]

--- Assets/Scripts/Greenflash.cs
+++ Assets/Scripts/Greenflash.cs
@@ -4,6 +4,9 @@ public class GreenLightOnEnter : MonoBehaviour
 {
     public Light greenLight;
 
+    private GetKey getKey;
+
+
     private void Start()
     {
         greenLight = GetComponentInChildren<Light>();
@@ -12,9 +15,19 @@ public class GreenLightOnEnter : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("Player")) // 플레이어가 충돌하면
         {
             greenLight.enabled = true; // 초록색 빛을 켜기
+            
+            if (getKey.collectedKeys == 0) //보유한 열쇠 개수가 1개
+            {
+                Debug.Log("입구" + getKey.collectedKeys);
+            }
+            if (getKey.collectedKeys == 1) //보유한 열쇠 개수가 2개
+            {
+                Debug.Log("입구" + getKey.collectedKeys);
+            }
         }
     }
 
@@ -23,6 +36,15 @@ public class GreenLightOnEnter : MonoBehaviour
         if (other.CompareTag("Player")) // 플레이어가 충돌하면
         {
             greenLight.enabled = false; // 초록색 빛을 끄기
+
+            if (getKey.collectedKeys == 0) //보유한 열쇠 개수가 1개
+            {
+                Debug.Log("출구" + getKey.collectedKeys);
+            }
+            if (getKey.collectedKeys == 1) //보유한 열쇠 개수가 2개
+            {
+                Debug.Log("출구" + getKey.collectedKeys);
+            }
         }
     }
 }
