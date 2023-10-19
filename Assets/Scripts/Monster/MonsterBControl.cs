4fb9fd07121faee272fb13c0e70d3421b471e0eb
koju2005 <141552941+koju2005@users.noreply.github.com>
2023-10-19 13:50:32 +0900

    [Add]Monster A,B 모델링,애니메이션, 함수 일부 수정 - kkh

--- Assets/Scripts/Monster/MonsterBControl.cs
+++ Assets/Scripts/Monster/MonsterBControl.cs
@@ -7,7 +7,7 @@ public class MonsterBControl : MonoBehaviour
 {
     // 이동할 목표 위치 좌표
     public Vector3 targetPosition;
-
+    public Material material;
     // 이동 속도
     public float moveSpeed = 3.0f;
 
@@ -26,17 +26,21 @@ public class MonsterBControl : MonoBehaviour
             targetPosition = PlayerController.instance.transform.position;
             // 현재 위치에서 목표 위치로 이동하는 방향 벡터 계산
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
-
             // 이동 속도와 Time.deltaTime을 사용하여 이동량을 조절
             Vector3 moveAmount = moveDirection * moveSpeed * Time.deltaTime;
 
             // 실제 이동 수행
             transform.Translate(moveAmount);
-
+            moveDirection.y = 0;
+            transform.rotation = Quaternion.LookRotation(moveDirection);
             // 목표 위치에 도달하면 이동 종료
             if (Vector3.Distance(transform.position, targetPosition) < 10.0f)
             {
                 moveSpeed = 1.0f;
+            }
+            if (Vector3.Distance(transform.position, targetPosition) < 3.0f)
+            {
+               this.GetComponent<MeshRenderer>().material = material;
             }
             if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
             {
